d5aac8235dba5c59ad1fe1c26e7948b27159045b
Ahmad Cooper <108217835+AhmadAC@users.noreply.github.com>
2023-12-27 13:14:43 +0800

    first commit

--- main.py
+++ main.py
@@ -8,20 +8,17 @@ from kivy.core.clipboard import Clipboard
 
 class MyApp(App):
     def build(self):
-        layout = BoxLayout(orientation='vertical', padding=[100,300,100,100])  # Added padding to layout
-
-        self.input = TextInput(hint_text='Enter a compound', multiline=False, halign="center", font_size=50, padding=[20, 270, 20, 20])  # Added padding to TextInput
+        layout = BoxLayout(orientation='vertical', padding=[100,300,100,100]) # Added padding to layout
+        self.input = TextInput(hint_text='Enter a compound', multiline=False, halign="center", font_size=50, padding=[20, 270, 20, 20]) # Added padding to TextInput
+        self.input.bind(on_text_validate=self.calculate) # Bind 'enter' key to calculate method
         layout.add_widget(self.input)
-
-        button_layout = BoxLayout(padding=[100,100,100,100])  # New layout for button
+        button_layout = BoxLayout(padding=[100,100,100,100]) # New layout for button
         button = Button(text='Calculate Molecular Mass', font_size=50)
         button.bind(on_press=self.calculate)
-        button_layout.add_widget(button)  # Add button to new layout
-        layout.add_widget(button_layout)  # Add new layout to main layout
-
+        button_layout.add_widget(button) # Add button to new layout
+        layout.add_widget(button_layout) # Add new layout to main layout
         self.label = Label(text='', halign="center", font_size=50)
         layout.add_widget(self.label)
-
         return layout
 
     def calculate(self, instance):
